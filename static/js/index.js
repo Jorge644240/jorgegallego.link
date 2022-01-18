e6ae0225a6c2e4f4e76b0ba71b1271c90e8894a7
Jorge Gallego <gallegojorge908@gmail.com>
2022-01-18 13:00:31 -0500

    Added functionality to all list elements on click

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,7 @@
 document.querySelector("#year").innerText = new Date().getFullYear();
+
+document.querySelectorAll("ul li").forEach(li => {
+    li.addEventListener("click", () => {
+        li.children[li.children.length-1].click();
+    });
+});
